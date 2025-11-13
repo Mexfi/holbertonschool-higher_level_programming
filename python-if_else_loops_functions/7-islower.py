360ae9394c07944bc18efe8d88ab13b8f8abbb90
Mexfi <cenab.1994@gmail.com>
2025-11-13 17:35:24 +0000

    sdsd

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def islower(c):
-    """Check if a character is lowercase."""
-    if len(c) != 1:
-        return False  # Optional: only consider single characters
-    return 97 <= ord(c) <= 122
+    """Check if a character is lowercase"""
+    ascii_val = ord(c)
+    return 97 <= ascii_val <= 122
