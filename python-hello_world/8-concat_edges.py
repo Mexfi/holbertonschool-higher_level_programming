8b9cc655a2ea35f61cbc26d2adfa4124e0d8d821
Mexfi <cenab.1994@gmail.com>
2025-11-13 16:55:50 +0000

    sdsd

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-text = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
-text = text[39:67] + text[107:112] + text[:6]
-print(text)
+str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
+str = str[39:67] + str[107:112] + str[:6]
+print(str)
