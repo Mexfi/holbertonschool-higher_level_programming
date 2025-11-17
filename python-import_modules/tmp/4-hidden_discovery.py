f8fa90b3070676249215dafbade4746e12ece2de
Mexfi <cenab.1994@gmail.com>
2025-11-17 11:02:20 +0000

    sdfdf

--- python-import_modules/tmp/4-hidden_discovery.py
+++ python-import_modules/tmp/4-hidden_discovery.py
@@ -2,7 +2,7 @@
 import hidden_4
 
 if __name__ == "__main__":
-    names = dir(hidden_4)
+    names = sorted(dir(hidden_4))
     for name in names:
         if not name.startswith("__"):
             print(name)
