44bfcf1d4f7a419e6930f9f472fc144f5802301c
Mexfi <cenab.1994@gmail.com>
2025-11-17 11:07:19 +0000

    sdsd

--- python-import_modules/tmp/4-hidden_discovery.py
+++ python-import_modules/tmp/4-hidden_discovery.py
@@ -2,7 +2,8 @@
 import hidden_4
 
 if __name__ == "__main__":
+    # dir() moduldakı bütün adları verir
     names = sorted(dir(hidden_4))
     for name in names:
-        if not name.startswith("__"):
+        if not name.startswith("__"):  # __ ilə başlayan adları çıxart
             print(name)
