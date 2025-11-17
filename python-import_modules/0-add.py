98e36fe3e27c393c4f3ac2a1a79381d93844a776
Mexfi <cenab.1994@gmail.com>
2025-11-17 10:40:54 +0000

    sdsd

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
-def add(a, b):
-    return a + b
-
-if __name__ == "__main__":
-    # This block will not be executed when imported
-    pass
-
 from add_0 import add
 
 a = 1
 b = 2
 
-print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+if __name__ == "__main__":
+    print("{} + {} = {}".format(a, b, add(a, b)))
