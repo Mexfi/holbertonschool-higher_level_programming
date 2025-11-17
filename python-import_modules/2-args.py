bfe6a3db7b123302c2bfd1ad102734d3c9be5fb1
Mexfi <cenab.1994@gmail.com>
2025-11-17 10:45:58 +0000

    sdsd

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,21 +1,14 @@
 #!/usr/bin/python3
 import sys
 
-def print_arguments():
-    """
-    Prints the number of and the list of command-line arguments.
-    Excludes the script name itself from the count and list.
-    """
-    args = sys.argv[1:]  # Exclude the script name (sys.argv[0])
-    num_args = len(args)
-
-    if num_args == 0:
-        print("0 arguments.")
-    else:
-        argument_word = "argument" if num_args == 1 else "arguments"
-        print(f"{num_args} {argument_word}:")
-        for i, arg in enumerate(args):
-            print(f"{i + 1}:{arg}")
-
 if __name__ == "__main__":
-    print_arguments()
+    argv = sys.argv
+    count = len(argv) - 1
+
+    if count == 0:
+        print("0 argument.")
+    else:
+        word = "argument" if count == 1 else "arguments"
+        print("{} {}:".format(count, word))
+        for i in range(1, len(argv)):
+            print("{}: {}".format(i, argv[i]))
