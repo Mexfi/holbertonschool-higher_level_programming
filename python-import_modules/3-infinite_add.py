e38247e1c50b292dbc84618dafc55d2b7acc2282
Mexfi <cenab.1994@gmail.com>
2025-11-17 10:51:41 +0000

    sdsfsd

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
-import sys
-
-def add_arguments():
-    """
-    Calculates the sum of all command-line arguments, treating them as integers.
-    Prints the result followed by a new line.
-    """
-    total_sum = 0
-    # sys.argv[0] is the script name, so we start from index 1 for arguments.
-    for arg in sys.argv[1:]:
-        total_sum += int(arg)
-    print(total_sum)
+mport sys
 
 if __name__ == "__main__":
-    add_arguments()
+    argv = sys.argv
+    total = 0
+
+    for i in range(1, len(argv)):
+        total += int(argv[i])
+
+    print(total)
