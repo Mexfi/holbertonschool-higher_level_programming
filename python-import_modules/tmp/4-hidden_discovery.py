f9794457f4fea5724fd63bc2c611aaa2a41e9323
Mexfi <cenab.1994@gmail.com>
2025-11-17 11:04:56 +0000

    asdads

--- python-import_modules/tmp/4-hidden_discovery.py
+++ python-import_modules/tmp/4-hidden_discovery.py
@@ -2,7 +2,7 @@
 import hidden_4
 
 if __name__ == "__main__":
-    names = dir(hidden_4)
-    filtered_names = sorted([name for name in names if not name.startswith("__")])
-    for name in filtered_names:
-        print(name)
+    names = sorted(dir(hidden_4))
+    for name in names:
+        if not name.startswith("__"):
+            print(name)
