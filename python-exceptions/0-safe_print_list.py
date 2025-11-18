b204fb67aea5cc3caa7d90f9e3ee1f5d0516614b
Mexfi <cenab.1994@gmail.com>
2025-11-18 10:57:50 +0000

    sdsd

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-# Safe Print List
-
 def safe_print_list(my_list=[], x=0):
     count = 0
+    index = 0
     try:
-        for item in my_list:
-            if count < x:
-                print(item, end="")
-                count += 1
-    except Exception:
+        while index < x:
+            # Print each element without newline, so elements are on the same line
+            print(my_list[index], end='')
+            count += 1
+            index += 1
+    except IndexError:
+        # End of list reached
         pass
-    print()
+    print()  # New line after printing elements
     return count
