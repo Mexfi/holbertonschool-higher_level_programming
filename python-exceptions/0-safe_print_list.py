d36fdb3ad4749e9f202e1d796590b137744a7316
Mexfi <cenab.1994@gmail.com>
2025-11-18 10:53:57 +0000

    sd

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     index = 0
     try:
         while index < x:
-            # Attempt to access each element
+            # Attempt to print each element
             print(my_list[index], end='')
             count += 1
             index += 1
