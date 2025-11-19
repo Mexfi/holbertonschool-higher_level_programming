6a35f9715033ef4b68ddbd904475fffc6e3800ab
Mexfi <cenab.1994@gmail.com>
2025-11-19 15:11:00 +0000

    dffd

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    result = None
     try:
         result = a / b
     except Exception:
@@ -8,4 +7,3 @@ def safe_print_division(a, b):
     finally:
         print("Inside result: {}".format(result))
     return result
-
