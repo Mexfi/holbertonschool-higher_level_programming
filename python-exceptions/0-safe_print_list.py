70ff319230b8e447932a3c63e1cdeedc4f8d2131
Mexfi <cenab.1994@gmail.com>
2025-11-19 11:16:49 +0000

    sdsd

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,13 +1,4 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
-    count = 0
-    try:
-        for i in range(x):
-            print(my_list[i], end='')  # Çap edilən elementlər bir yerdə göstərilir
-            count += 1
-    except IndexError:
-        pass  # Siyahı bitərsə, sadəcə davam et
-
-    print()  # Yeni sətirə keçmək üçün bu buradadır
-    return count
-
+def safe_print_list (my_list=[1,2,3,4,5,]
+print ("1,2,3,4,5"
+)
