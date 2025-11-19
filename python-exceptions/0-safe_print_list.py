ed1397a1aa49fec88b219dd780c888205522d552
Mexfi <cenab.1994@gmail.com>
2025-11-19 11:30:35 +0000

    sdsd

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
-def safe_print_list (my_list=[1,2,3,4,5,]
-print ["1,2,3,4,5"]
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    try:
+        for i in range(x):
+            try:
+                print(my_list[i], end="")
+                count += 1
+            except Exception:
+                break
+    finally:
+        print()  # New line after printing
+    return count
