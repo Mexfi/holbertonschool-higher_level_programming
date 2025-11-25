198049bebc283c3d4a24592b903f7c25ec95166f
Mexfi <cenab.1994@gmail.com>
2025-11-25 04:53:20 +0000

    asas

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""Module that returns a list of available attributes and methods of an object."""
-
+"""Module that returns a list of available attributes and methods
+of an object."""
 
 def lookup(obj):
     """Returns the list of available attributes and methods of an object."""
