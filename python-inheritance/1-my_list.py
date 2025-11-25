8389504ad3e3673a1fec4588ebfd8260ab7c7f77
Mexfi <cenab.1994@gmail.com>
2025-11-25 07:23:11 +0000

    sdsd

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """MyList class that inherits from list and has a method to print a sorted list."""
 
-
 class list(My_lists):
     """A class that inherits from the built-in list class and adds a print_sorted method."""
 
