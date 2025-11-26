e08043bbc5798a62923b80fc80afe9e39d9466d8
Mexfi <cenab.1994@gmail.com>
2025-11-26 22:24:29 +0000

    dfdf

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Module: 10-square
+Contains class Square that inherits from Rectangle
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """A class Square that inherits from Rectangle"""
+
+    def __init__(self, size):
+        """
+        Initialize Square with size
+
+        Args:
+            size: size of square (positive integer)
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
