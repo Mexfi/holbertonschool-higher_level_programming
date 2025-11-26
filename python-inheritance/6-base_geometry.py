51059eb6a60c4db5070023ab29c5df77629b34cf
Mexfi <cenab.1994@gmail.com>
2025-11-26 21:43:25 +0000

    dfd

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module: 6-base_geometry
+Contains class BaseGeometry with area method
+"""
+
+
+class BaseGeometry:
+    """A class BaseGeometry with area method"""
+
+    def area(self):
+        """Raises an Exception with message 'area() is not implemented'"""
+        raise Exception("area() is not implemented")
