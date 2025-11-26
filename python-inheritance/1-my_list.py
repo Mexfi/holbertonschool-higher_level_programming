b9658868b6ac79fd74036600f28b93993b1a2362
Mexfi <cenab.1994@gmail.com>
2025-11-26 21:29:47 +0000

    sdsd

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -7,7 +7,7 @@ Contains class MyList that inherits from list
 
 class MyList(list):
     """A class that inherits from list"""
-    
+
     def print_sorted(self):
         """Prints the list sorted in ascending order"""
         print(sorted(self))
