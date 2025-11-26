843e69069e9a8416a745a06e53cc19501ec530b4
Mexfi <cenab.1994@gmail.com>
2025-11-26 21:40:51 +0000

    dfdf

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Module: 4-inherits_from
+Contains function inherits_from
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Returns True if the object is an instance of a class that inherited
+    (directly or indirectly) from the specified class; otherwise False.
+
+    Args:
+        obj: The object to check
+        a_class: The class to compare against
+
+    Returns:
+        bool: True if obj inherits from a_class, False otherwise
+    """
+    return isinstance(obj, a_class) and type(obj) is not a_class
