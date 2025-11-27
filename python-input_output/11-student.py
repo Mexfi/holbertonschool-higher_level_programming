a5e8dcecb5f17c5ef47f565ffac771a9b5f5d9eb
Mexfi <cenab.1994@gmail.com>
2025-11-27 18:20:27 +0000

    asas

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python3
-"""Pascal's Triangle"""
-
-
 def pascal_triangle(n):
-    """Returns a list of lists of integers representing Pascal's triangle of n"""
+    """Returns a list of lists of integers representing Pascal's triangle"""
     if n <= 0:
         return []
-    
-    triangle = [[1]]
-    
+
+    triangle = [[1]]  # first row
+
     for i in range(1, n):
-        row = [1]
-        prev_row = triangle[i-1]
-        
+        prev = triangle[-1]
+        row = [1]  # first element
+
+        # middle values
         for j in range(1, i):
-            row.append(prev_row[j-1] + prev_row[j])
-        
-        row.append(1)
+            row.append(prev[j-1] + prev[j])
+
+        row.append(1)  # last element
         triangle.append(row)
-    
+
     return triangle
