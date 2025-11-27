4262f6aa5e282f390884dfdd43135f99614ac67e
Mexfi <cenab.1994@gmail.com>
2025-11-27 18:32:55 +0000

    ass

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,23 +1,27 @@
 #!/usr/bin/python3
-"""Module for generating Pascal's triangle"""
+"""
+Module that defines a function to generate Pascal's triangle.
+"""
 
 
 def pascal_triangle(n):
-"""Returns a list of lists of integers representing
-    the Pascal’s triangle of n."""
+    """
+    Returns a list of lists of integers representing
+    the Pascal’s triangle of n.
+    """
     if n <= 0:
         return []
 
     triangle = [[1]]
 
     for i in range(1, n):
-        prev = triangle[-1]
-        row = [1]
+        prev_row = triangle[-1]
+        new_row = [1]
 
         for j in range(1, i):
-            row.append(prev[j - 1] + prev[j])
+            new_row.append(prev_row[j - 1] + prev_row[j])
 
-        row.append(1)
-        triangle.append(row)
+        new_row.append(1)
+        triangle.append(new_row)
 
     return triangle
