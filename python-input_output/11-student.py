7e4f14dfb46f5618093664ad30a0e69b26be5d68
Mexfi <cenab.1994@gmail.com>
2025-11-27 18:27:39 +0000

    ddd

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,31 +1,21 @@
 #!/usr/bin/python3
-"""Pascal's Triangle Module"""
+"""Defines a Pascal's Triangle function."""
 
 
 def pascal_triangle(n):
-    """Returns a list of lists of integers representing Pascal's triangle of n
-    
-    Args:
-        n (int): Number of rows in Pascal's triangle
-        
-    Returns:
-        list: List of lists representing Pascal's triangle
+    """Represents Pascal's Triangle of size n.
+
+    Returns a list of lists of integers representing the triangle.
     """
     if n <= 0:
         return []
-    
-    triangle = []
-    
-    for i in range(n):
-        row = [1]  # Every row starts with 1
-        
-        # Calculate middle elements for rows after the first
-        if i > 0:
-            prev_row = triangle[i-1]
-            for j in range(1, i):
-                row.append(prev_row[j-1] + prev_row[j])
-            row.append(1)  # Every row ends with 1
-        
-        triangle.append(row)
-    
-    return triangle
+
+    triangles = [[1]]
+    while len(triangles) != n:
+        tri = triangles[-1]
+        tmp = [1]
+        for i in range(len(tri) - 1):
+            tmp.append(tri[i] + tri[i + 1])
+        tmp.append(1)
+        triangles.append(tmp)
+    return triangles
