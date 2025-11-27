0df1edb54dd7308f0c1abd2a28d0e601c8780633
Mexfi <cenab.1994@gmail.com>
2025-11-27 18:14:27 +0000

    asd

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -7,15 +7,16 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    triangle = []
+    triangle = [[1]]
 
-    for i in range(n):
-        row = []
-        for j in range(i + 1):
-            if j == 0 or j == i:
-                row.append(1)
-            else:
-                row.append(triangle[i-1][j-1] + triangle[i-1][j])
+    for i in range(1, n):
+        row = [1]
+        prev_row = triangle[i-1]
+
+        for j in range(1, i):
+            row.append(prev_row[j-1] + prev_row[j])
+
+        row.append(1)
         triangle.append(row)
 
     return triangle
