b007b3183ae32ffb67c1041915f5990059c9fe26
Mexfi <cenab.1994@gmail.com>
2025-11-27 12:53:08 +0000

    sdsd

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python3
 """
-Script that adds all command-line arguments to a Python list
-and saves them to a JSON file.
+Script that adds command-line arguments to a Python list and saves
+the list in a JSON file named add_item.json.
+
+Uses:
+    - save_to_json_file() from 5-save_to_json_file.py
+    - load_from_json_file() from 6-load_from_json_file.py
+
+The file is created if it does not exist.
 """
 
 import sys
-from 5-save_to_json_file import save_to_json_file
-from 6-load_from_json_file import load_from_json_file
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 
 try:
-    items = load_from_json_file(filename)
+    data = load_from_json_file(filename)
 except Exception:
-    items = []
+    data = []
 
-items.extend(sys.argv[1:])
+data.extend(sys.argv[1:])
 
-save_to_json_file(items, filename)
+save_to_json_file(data, filename)
