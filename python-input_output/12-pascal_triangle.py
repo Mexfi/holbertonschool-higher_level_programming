06fbfef3c2ce93e0c877c97ae2ded5c017682ec6
Mexfi <cenab.1994@gmail.com>
2025-11-27 18:11:23 +0000

    asas

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -4,16 +4,15 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    triangle = [[1]]
+    triangle = []
 
-    for i in range(1, n):
-        row = [1]  # First element is always 1
-
-        # Calculate middle elements
-        for j in range(1, i):
-            row.append(triangle[i-1][j-1] + triangle[i-1][j])
-
-        row.append(1)  # Last element is always 1
+    for i in range(n):
+        row = []
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                row.append(1)
+            else:
+                row.append(triangle[i-1][j-1] + triangle[i-1][j])
         triangle.append(row)
 
     return triangle
